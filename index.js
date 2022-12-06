9b348798a7571efc2a259346f7cb56f28258574e
beesanne <beesanne.kurzum@gmail.com>
2022-12-06 17:00:46 -0500

    connecting db... not working

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 const userRoutes = require('./server/routes/users');
+const noteRoutes = require('./server/routes/note');
+
 
 app.use(express.json());
+
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/login.html")));
 
 //CORS middleware
 app.use(function(req, res, next) {
@@ -14,6 +21,8 @@ app.use(function(req, res, next) {
 });
 
 app.use("/users", userRoutes);
+app.use("/note", noteRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
--- server/models/dbconn.js
+++ server/models/dbconn.js
@@ -1,20 +1,34 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+
 const con = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB
 })
 
-const query = (sql, binding)=> {
-    return new Promise( (resolve,reject)=> {
-        con.query(sql, binding, (err, result, fields)=>{ 
-        if(err) reject(err);
-        resolve(result);
+const query = (sql, binding) => {
+  return new Promise((resolve, reject) => {
+    con.query(sql, binding, (err, result, fields)=> {
+      if(err) reject(err);
+      resolve(result);
     })
-    })
+  })
 }
 
-module.exports={con, query};
+
+con.query(function(err){
+    if(err) throw err;
+    console.log("Connected");
+    con.query("CREATE DATABASE IF NOT EXISTS WDP_Project", function(err, result){
+        if(err) throw err;
+        console.log("Database Created");
+    });
+});
+
+module.exports = {con, query};
+
+
+
