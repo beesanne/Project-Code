84260edf4914cec2a88854e0b472295ce53c7757
beesanne <beesanne.kurzum@gmail.com>
2022-12-06 17:03:37 -0500

    fixed syntax

--- server/routes/note.js
+++ server/routes/note.js
@@ -1,44 +1,42 @@
-const express = require("express");
-const Note = require("../models/note");
+const express = require('express');
+const Note = require('../models/note');
 const router = express.Router();
 
 router
-
-.post('/create', async (req, res) => {
-  try {
-    let note = await Note.createNote(req.body);
-    res.send({ ...note });
-  } catch (err) {
-    res.status(401).send({ message: err.message });
-  }
-})
-
-.post('/read', async (req, res) => {
-    try{
-        let note = await Note.getNote(req.body);
-        res.send({...note})
-    } catch(err){
-        res.status(401).send({ message: err.message });
+  .post('/create', async (req, res) => {
+    try {
+      let note = await Note.createNote(req.body);
+      res.send({ ...note });
+    } catch (err) {
+      res.status(401).send({ message: err.message });
     }
-})
+  })
 
-.put('/edit', async (req, res) => {
+  .post('/read', async (req, res) => {
+    try {
+      let note = await Note.getNote(req.body);
+      res.send({ ...note });
+    } catch (err) {
+      res.status(401).send({ message: err.message });
+    }
+  })
+
+  .put('/edit', async (req, res) => {
     try {
       let note = await Note.editNote(req.body);
-      res.send({...note})
-    } catch(err) {
-      res.status(401).send({message: err.message})
+      res.send({ ...note });
+    } catch (err) {
+      res.status(401).send({ message: err.message });
     }
   })
 
   .delete('/delete', async (req, res) => {
     try {
       Note.deleteNote(req.body);
-      res.send({success: "Note Deleted"})
-    } catch(err) {
-      res.status(401).send({message: err.message})
+      res.send({ success: "Note Deleted" });
+    } catch (err) {
+      res.status(401).send({ message: err.message });
     }
-  })
-
+  });
 
 module.exports = router;
