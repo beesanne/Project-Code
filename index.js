221fcd51f3be24722627b445536f2b6bd91f238e
beesanne <beesanne.kurzum@gmail.com>
2022-12-06 17:14:55 -0500

    commented out dirname code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 
 const userRoutes = require('./server/routes/users');
 const noteRoutes = require('./server/routes/note');
 
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(express.static(__dirname + "/public"));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/login.html")));
+// app.use(express.static(__dirname + "/public"));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/login.html")));
 
 //CORS middleware
 app.use(function(req, res, next) {
